e5fa6b361cd9d55192ac5bcbc233093767110ef6
khanhcv06 <khanhcv06@gmail.com>
2025-07-13 19:20:48 +0700

    BTVNJV1

---
+++ BTVN01/src/BTVNT1.java
@@ -0,0 +1,8 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class BTVNT1 {
+    public static void main(String[] args) {
+        System.out.println("Hello moi nguoi");
+        System.out.println("Day la BTVN cua minh");
+    }
+}
